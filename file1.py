2bd3e33098813180b8bde6cde7a12d1ed84a15f1
ARAVINDPALURU <162983689+ARAVINDPALURU@users.noreply.github.com>
2024-03-11 16:21:39 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,63 @@
+
+#print ( hello world )
+print ("hello world ")
+,,,
+1. commenting is used for making documention in a python tile
+2. when a line is commented, that line will not be considered as apart of code
+3. when the program is executed , commented line will not be in the part of execution
+ NOTE - sinle line commenting can be done using 3 at the begining of the line
+ ex : # print ( " hello " )
+      multiple line commenting can be done using # at the begining of the line
+      ex : """
+print (" hello world")
+print (" hello world")
+print (" hello world")
+print (" hello world")
+print (" hello world")
+"""
+import this  
+print (this)
+
+import antigravity
+
+'''
+Bargained characters
+1.controls and data structures - ABC language
+2.strings and its formats - from c
+3.regular expressions - pearl
+4.exception handling - module 3
+5.magic methods from - fortron
+'''
+
+'''
+what kind of language is pythan
+pythan is a
+> object oriented programming language
+>dynamically typed language
+>scripting language
+>interpreted language
+>compiled language
+>functional programming language
+'''
+
+'''
+>Cpython - written in C, most common implemetation of pythan.
+>Jython - java imp-lementation of python
+>Ironpython - implememtation of python programming language which is tightly intigrated with  . NET
+>Brython - browser python ,runs in browsers
+>Ruby python - bridge betweeen ruby and interpreters
+>Micro python - runs in micro controllers
+,,,
+
+,,,
+# ! ---> Variables
+#?Variable is a name which is used to identify the value of variable
+#?Which is stored in a specific memory location
+
+# !Rules to define variaboles
+#1. ) Variable have to satrt with only alphabets ,underscores
+#2. ) The char following the 1st char can be alphabets , numbers , underscores
+#3. ) No spaces are allowed
+#4. ) No special characters like !@#$%^&*()+
+#5. ) Case sensitive
+#6. ) Keywords are not allowed as variables
